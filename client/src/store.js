9fb2b70cd4be148034abfbdb2c666abc0ad86505
V S Bharath Chandra <bharathchandra630@gmail.com>
2019-05-23 22:34:30 +0530

    trying

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,27 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
+//we dont have initial state for our create store
 const initialState = {};
+
+let devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production") {
+  devTools = a => a;
+}
 
 const middleware = [thunk];
 
+//array=reducer,initialstate
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
   )
 );
 
 export default store;
+applyMiddleware(...middleware);
