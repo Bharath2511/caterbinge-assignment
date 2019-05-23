773cbfc79660495b8e855872ac0be7dd1bf76d60
V S Bharath Chandra <bharathchandra630@gmail.com>
2019-05-23 21:50:10 +0530

    prepared for deployment

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
-module.exports = {
-    mongoURI: "mongodb://bharath:8686bharath@ds131480.mlab.com:31480/reg-log",
-    secretOrKey: 'secret'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod')
+}
+else {
+    module.exports = require('./keys_dev')
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path')
 
 const users = require('./routes/api/users')
 
@@ -23,6 +24,16 @@ app.use(passport.initialize());
 require('./config/passport')(passport)
 
 app.use('/api/users', users)
+
+//server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+        res.send(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 const port = 5001 || process.env.PORT
 
